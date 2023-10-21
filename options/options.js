c3d7aeef82bfe6e0ce6b60fd4e99e901ad19babe
Crowdbotics <team@crowdbotics.com>
2023-10-21 14:15:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weather-44085",
+  url: "https://weather-44085.botics.co",
+  api: "https://weather-44085.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
